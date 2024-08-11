39073031c3ccafee5e1b5bf93157f0428c6c9da2
ganesh kumar <ganeshargjl@gmail.com>
2024-08-11 18:49:01 +0530

    Update

---
+++ src/main/java/com/retailer/rewards/controller/CustomGlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.retailer.rewards.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.retailer.rewards.exceptions.CustomErrorResponse;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler  
+ 
+	{
+	    @ExceptionHandler(RuntimeException.class)
+	    public ResponseEntity<CustomErrorResponse> customHandleNotFound(Exception ex) {
+	        CustomErrorResponse errors = new CustomErrorResponse();
+	        errors.setError("Custom Internal Server Error Message");
+	        errors.setMessage(ex.getMessage());
+
+	        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+
---
+++ src/main/java/com/retailer/rewards/repository/TransactionRepository.java
@@ -0,0 +1,22 @@
+package com.retailer.rewards.repository;
+
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.retailer.rewards.entity.Transaction;
+
+
+
+@Repository
+@Transactional
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+	public List<Transaction> findAllByCustomerId(Long customerId);
+	public List<Transaction> findAllByCustomerIdAndTransactionDateBetween(Long customerId,Timestamp from, Timestamp to);
+	
+
+}
